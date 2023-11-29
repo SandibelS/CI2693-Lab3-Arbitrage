3297a01b45c520e9232341389e3b2663876e9242
SandibelS <17-10614@usb.ve>
2023-11-29 14:14:23 -0500

    Added aux class

---
+++ Triple.java
@@ -0,0 +1,11 @@
+public class Triple <T>{
+    public T val1;
+    public T val2;
+    public double cost;
+
+    public Triple(T val1, T val2, double cost){
+        this.val1 = val1;
+        this.val2 = val2;
+        this.cost = cost;
+    }
+}
